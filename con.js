26bcc2ff7ead8a722f399863b0a282a34202b0f4
S M Tufayal Ahammad <120366739+tufayal10@users.noreply.github.com>
2025-07-10 23:29:21 +0600

    Update con.js

--- con.js
+++ con.js
@@ -1,21 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAI0YnHW7A-gURWdKfgG4A2USm7XgPAuOM",
   authDomain: "chess-7aa3c.firebaseapp.com",
+  databaseURL: "https://chess-7aa3c-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "chess-7aa3c",
-  storageBucket: "chess-7aa3c.firebasestorage.app",
+  storageBucket: "chess-7aa3c.appspot.com",
   messagingSenderId: "931547414074",
-  appId: "1:931547414074:web:9c9501b3bc5818583f7f03",
-  measurementId: "G-1L6MYTEP23"
+  appId: "1:931547414074:web:9c9501b3bc5818583f7f03"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const database = getDatabase(app);
+export { database };
