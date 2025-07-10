0a4cf803c227b4a5036bf0363acf38137ee72dfd
S M Tufayal Ahammad <120366739+tufayal10@users.noreply.github.com>
2025-07-10 22:15:55 +0600

    Add files via upload

---
+++ con.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAI0YnHW7A-gURWdKfgG4A2USm7XgPAuOM",
+  authDomain: "chess-7aa3c.firebaseapp.com",
+  projectId: "chess-7aa3c",
+  storageBucket: "chess-7aa3c.firebasestorage.app",
+  messagingSenderId: "931547414074",
+  appId: "1:931547414074:web:9c9501b3bc5818583f7f03",
+  measurementId: "G-1L6MYTEP23"
+};
+firebase.initializeApp(firebaseConfig);
